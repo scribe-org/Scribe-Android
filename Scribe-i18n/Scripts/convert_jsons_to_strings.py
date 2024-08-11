672730ed8e1da29d405708d503484f92f4d3f370
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2024-08-11 13:20:12 +0200

    Squashed 'app/src/main/assets/i18n/' content from commit d9a35d7
    git-subtree-dir: app/src/main/assets/i18n
    git-subtree-split: d9a35d73364d49ba1d5dae92df2171b660a84384

---
+++ Scribe-i18n/Scripts/convert_jsons_to_strings.py
@@ -0,0 +1,39 @@
+"""
+Converts from Scribe-i18n localization JSON files to the Strings files.
+
+Usage:
+    python3 Scribe-i18n/Scripts/convert_jsons_to_strings.py
+"""
+import os
+import json
+
+directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+dir_list = os.listdir(directory)
+languages = sorted(
+    [file.replace(".json", "") for file in dir_list if file.endswith(".json")]
+)
+values_directory = os.path.join(directory, 'values')
+os.makedirs(values_directory, exist_ok=True)
+
+for lang in languages:
+    lang_dir = os.path.join(values_directory, lang)
+    os.makedirs(lang_dir, exist_ok=True)
+
+    xml_path = os.path.join(lang_dir,"string.xml")
+    with open(xml_path, "w") as xml_file:
+        xml_file.write('<?xml version="1.0" encoding="utf-8"?>\n')
+        xml_file.write('<resources>\n')
+
+        json_path = os.path.join(directory, f"{lang}.json")
+        with open(json_path, "r") as json_file:
+            json_data = json_file.read()
+            json_data = json.loads(json_data)
+
+            for key, value in json_data.items():
+                xml_file.write(f'    <string name="{key}">{value}</string>\n')
+
+        xml_file.write('</resources>\n')
+
+print(
+    "Scribe-i18n localization JSON files successfully converted to the strings file."
+)
