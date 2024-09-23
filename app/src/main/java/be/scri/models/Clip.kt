ce1c4e521f88fde8228105a7b73951ca2735703e
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2024-09-23 08:33:59 +0200

    Re-add spacing to Clip.kt to fix detekt error

--- app/src/main/java/be/scri/models/Clip.kt
+++ app/src/main/java/be/scri/models/Clip.kt
@@ -5,8 +5,8 @@ import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tablename="clips", indices=[(Index(value=["id"], unique=true))])
+@Entity(tablename = "clips", indices = [(Index(value = ["id"], unique=true))])
 data class Clip(
     @PrimaryKey(autoGenerate = true) var id: Long?,
-    @ColumnInfo(name="value") var value: String,
+    @ColumnInfo(name = "value") var value: String,
 ) : ListItem()
