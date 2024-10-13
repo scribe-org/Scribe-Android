9bb87827f278b85b63fd6c9e9621e02b90816bd2
Radovan Zajac <radovan.zajac.2@student.tuke.sk>
2024-10-13 15:20:08 +0200

    App hints - lint HintUtils.kt

--- app/src/main/java/be/scri/helpers/HintUtils.kt
+++ app/src/main/java/be/scri/helpers/HintUtils.kt
@@ -1,13 +1,11 @@
 package be.scri.helpers
 
 import android.content.Context
-import android.util.Log
 
 class HintUtils {
     companion object {
         fun resetHints(context: Context) {
             val sharedPref = context.getSharedPreferences("app_preferences", Context.MODE_PRIVATE)
-            Log.d("HintUtils", "Resetting hints")
             with(sharedPref.edit()) {
                 putBoolean("hint_shown_main", false)
                 putBoolean("hint_shown_settings", false)
