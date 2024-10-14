009abba2e96a154d4200b2909166b48d1e3a1c72
chris <chris.w.witt@gmail.com>
2024-10-14 09:31:45 -0400

    Creating separate extensions file for Context as it's related to themes specifically.

--- app/src/main/java/be/scri/extensions/ContextStyling.kt
+++ app/src/main/java/be/scri/extensions/ContextStyling.kt
@@ -3,17 +3,9 @@ package be.scri.extensions
 import android.content.ComponentName
 import android.content.Context
 import android.content.pm.PackageManager
-import android.content.res.Configuration
-import android.graphics.Color
 import android.view.ViewGroup
-import androidx.loader.content.CursorLoader
 import be.scri.R
-import be.scri.helpers.DARK_GREY
-import be.scri.helpers.INVALID_NAVIGATION_BAR_COLOR
-import be.scri.helpers.MyContentProvider
 import be.scri.helpers.appIconColorStrings
-import be.scri.helpers.ensureBackgroundThread
-import be.scri.models.SharedTheme
 import be.scri.views.MyTextView
 
 fun Context.getColorWithDefault(color: Int, defaultColor: Int) =
@@ -55,39 +47,6 @@ fun Context.updateTextColors(viewGroup: ViewGroup) {
             is ViewGroup -> updateTextColors(view)
         }
     }
-}
-
-fun Context.isBlackAndWhiteTheme() = baseConfig.textColor == Color.WHITE && baseConfig.primaryColor == Color.BLACK && baseConfig.backgroundColor == Color.BLACK
-
-fun Context.isWhiteTheme() = baseConfig.textColor == DARK_GREY && baseConfig.primaryColor == Color.WHITE && baseConfig.backgroundColor == Color.WHITE
-
-fun Context.isUsingSystemDarkTheme() = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_YES != 0
-
-fun Context.getSharedTheme(callback: (sharedTheme: SharedTheme?) -> Unit) {
-    val cursorLoader = getMyContentProviderCursorLoader()
-    ensureBackgroundThread {
-        callback(getSharedThemeSync(cursorLoader))
-    }
-}
-
-fun Context.getSharedThemeSync(cursorLoader: CursorLoader): SharedTheme? {
-    val cursor = cursorLoader.loadInBackground()
-    cursor?.use {
-        if (cursor.moveToFirst()) {
-            try {
-                val textColor = cursor.getIntValue(MyContentProvider.COL_TEXT_COLOR)
-                val backgroundColor = cursor.getIntValue(MyContentProvider.COL_BACKGROUND_COLOR)
-                val primaryColor = cursor.getIntValue(MyContentProvider.COL_PRIMARY_COLOR)
-                val accentColor = cursor.getIntValue(MyContentProvider.COL_ACCENT_COLOR)
-                val appIconColor = cursor.getIntValue(MyContentProvider.COL_APP_ICON_COLOR)
-                val navigationBarColor = cursor.getIntValueOrNull(MyContentProvider.COL_NAVIGATION_BAR_COLOR) ?: INVALID_NAVIGATION_BAR_COLOR
-                val lastUpdatedTS = cursor.getIntValue(MyContentProvider.COL_LAST_UPDATED_TS)
-                return SharedTheme(textColor, backgroundColor, primaryColor, appIconColor, navigationBarColor, lastUpdatedTS, accentColor)
-            } catch (e: Exception) {
-            }
-        }
-    }
-    return null
 }
 
 fun Context.checkAppIconColor() {
