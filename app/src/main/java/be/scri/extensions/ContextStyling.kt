656240729e06ddec52e09fd37971fb6193828c8e
Arpita kesharwani <107834813+KesharwaniArpita@users.noreply.github.com>
2024-10-19 17:52:03 +0530

    Create ContextStyling.kt

---
+++ app/src/main/java/be/scri/extensions/ContextStyling.kt
@@ -0,0 +1,43 @@
+
+package be.scri.extensions
+
+import android.content.Context
+import android.content.res.Configuration
+import android.graphics.Color
+import be.scri.R
+import be.scri.helpers.DARK_GREY
+
+// handle system default theme (Material You) specially as the color is taken from the system, not hardcoded by us
+fun Context.getProperTextColor() =
+    if (baseConfig.isUsingSystemTheme) {
+        resources.getColor(R.color.you_neutral_text_color, theme)
+    } else {
+        baseConfig.textColor
+    }
+
+fun Context.getProperKeyColor() =
+    if (baseConfig.isUsingSystemTheme) {
+        resources.getColor(R.color.you_neutral_text_color, theme)
+    } else {
+        baseConfig.keyColor
+    }
+
+fun Context.getProperBackgroundColor() =
+    if (baseConfig.isUsingSystemTheme) {
+        resources.getColor(R.color.you_background_color, theme)
+    } else {
+        baseConfig.backgroundColor
+    }
+
+fun Context.getProperPrimaryColor() =
+    when {
+        baseConfig.isUsingSystemTheme -> resources.getColor(R.color.you_primary_color, theme)
+        isWhiteTheme() || isBlackAndWhiteTheme() -> baseConfig.accentColor
+        else -> baseConfig.primaryColor
+    }
+
+fun Context.isBlackAndWhiteTheme() = baseConfig.textColor == Color.WHITE && baseConfig.primaryColor == Color.BLACK && baseConfig.backgroundColor == Color.BLACK
+
+fun Context.isWhiteTheme() = baseConfig.textColor == DARK_GREY && baseConfig.primaryColor == Color.WHITE && baseConfig.backgroundColor == Color.WHITE
+
+fun Context.isUsingSystemDarkTheme() = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_YES != 0
