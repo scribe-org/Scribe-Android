f642bc2f66589cf05b10b98f9874878f7f80a1e6
Arpita kesharwani <107834813+KesharwaniArpita@users.noreply.github.com>
2024-10-19 17:51:29 +0530

    Create Context.kt

---
+++ app/src/main/java/be/scri/extensions/Context.kt
@@ -0,0 +1,24 @@
+package be.scri.extensions
+
+import android.content.Context
+import android.graphics.Color
+import be.scri.R
+import be.scri.helpers.Config
+
+val Context.config: Config get() = Config.newInstance(applicationContext)
+
+fun Context.getStrokeColor(): Int =
+    if (config.isUsingSystemTheme) {
+        if (isUsingSystemDarkTheme()) {
+            resources.getColor(R.color.md_grey_800, theme)
+        } else {
+            resources.getColor(R.color.md_grey_400, theme)
+        }
+    } else {
+        val lighterColor = getProperBackgroundColor().lightenColor()
+        if (lighterColor == Color.WHITE || lighterColor == Color.BLACK) {
+            resources.getColor(R.color.divider_grey, theme)
+        } else {
+            lighterColor
+        }
+    }
