b6b22c5330d5b6e9b6558f85fea9c89f7d8df4de
Arpita kesharwani <107834813+KesharwaniArpita@users.noreply.github.com>
2024-10-19 17:53:09 +0530

    Create Drawable.kt

---
+++ app/src/main/java/be/scri/extensions/Drawable.kt
@@ -0,0 +1,6 @@
+package be.scri.extensions
+
+import android.graphics.PorterDuff
+import android.graphics.drawable.Drawable
+
+fun Drawable.applyColorFilter(color: Int) = mutate().setColorFilter(color, PorterDuff.Mode.SRC_IN)
