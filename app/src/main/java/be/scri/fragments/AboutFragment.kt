332bba6c14564379e3de8ac55064980ec9835a5f
chris <chris.w.witt@gmail.com>
2024-10-23 20:12:33 -0400

    Creating one single function to control action bar visibility.

--- app/src/main/java/be/scri/fragments/AboutFragment.kt
+++ app/src/main/java/be/scri/fragments/AboutFragment.kt
@@ -32,7 +32,7 @@ class AboutFragment : ScribeFragment("About") {
             }
         callback.isEnabled = true
         (requireActivity() as MainActivity).setActionBarTitle(R.string.app_about_title)
-        (requireActivity() as MainActivity).unsetActionBarLayoutMargin()
+        (requireActivity() as MainActivity).setActionBarVisibility(false)
         (requireActivity() as MainActivity).setActionBarButtonInvisible()
         return binding.root
     }
