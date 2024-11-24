a8eb75767afdf7456fe4160bd38c691ec6156f09
angrezichatterbox <gouthammohanraj@gmail.com>
2024-11-24 14:42:00 +0530

    feat:Added the base template for the settings page

--- app/src/main/java/be/scri/fragments/SettingsFragment.kt
+++ app/src/main/java/be/scri/fragments/SettingsFragment.kt
@@ -1,34 +1,103 @@
 package be.scri.fragments
 
+import SettingsScreen
+import android.content.Context
+import android.content.Intent
+import android.content.res.Configuration
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+import android.provider.Settings.ACTION_APP_LOCALE_SETTINGS
+import android.provider.Settings.ACTION_INPUT_METHOD_SETTINGS
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.activity.addCallback
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.compose.ui.platform.ComposeView
 import be.scri.R
 import be.scri.activities.MainActivity
 import be.scri.helpers.PreferencesHelper
-import be.scri.ui.screens.SettingsScreen
 import be.scri.ui.theme.ScribeTheme
 
 class SettingsFragment : ScribeFragment("Settings") {
+    private var isKeyboardInstalled = false
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View =
-        ComposeView(requireContext()).apply {
+    ): View {
+        setupBackPress()
+        setupActionBar()
+
+        return ComposeView(requireContext()).apply {
             setContent {
+                val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+                val isSystemDarkMode = currentNightMode == Configuration.UI_MODE_NIGHT_YES
+
                 ScribeTheme(
-                    useDarkTheme =
-                        PreferencesHelper.getUserDarkModePreference(requireContext())
-                            == AppCompatDelegate.MODE_NIGHT_YES,
+                    useDarkTheme = PreferencesHelper.getUserDarkModePreference(requireContext())
+                        == AppCompatDelegate.MODE_NIGHT_YES,
                 ) {
-                    SettingsScreen()
+                    SettingsScreen(
+                        onLanguageSelect = ::selectLanguage,
+                        onDarkModeChange = ::setLightDarkMode,
+                        onInstallKeyboard = ::navigateToKeyboardSettings,
+                        isKeyboardInstalled = isKeyboardInstalled,
+                        defaultDarkMode = isSystemDarkMode
+                    )
                 }
             }
         }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkKeyboardInstallation()
+    }
+
+    private fun setupBackPress() {
+        val callback = requireActivity().onBackPressedDispatcher.addCallback(this) {
+            parentFragmentManager.popBackStack()
+        }
+        callback.isEnabled = true
+    }
+
+    private fun setupActionBar() {
+        (requireActivity() as MainActivity).apply {
+            setActionBarTitle(R.string.app_settings_title)
+            setActionBarVisibility(false)
+            setActionBarButtonVisibility(false)
+            supportActionBar?.title = getString(R.string.app_settings_title)
+        }
+    }
+
+    private fun checkKeyboardInstallation() {
+        val imm = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        isKeyboardInstalled = imm.enabledInputMethodList.any {
+            it.packageName == "be.scri.debug"
+        }
+    }
+
+    private fun selectLanguage() {
+        val packageName = requireActivity().packageName
+        val intent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            Intent(ACTION_APP_LOCALE_SETTINGS)
+        } else {
+            Intent(ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                data = Uri.fromParts("package", packageName, null)
+                putExtra(
+                    "android.intent.extra.SHOW_FRAGMENT",
+                    "com.android.settings.localepicker.LocaleListEditor"
+                )
+            }
+        }
+        intent.data = Uri.fromParts("package", packageName, null)
+        startActivity(intent)
+    }
 
     private fun setLightDarkMode(isDarkMode: Boolean) {
         PreferencesHelper.setLightDarkModePreference(requireContext(), isDarkMode)
@@ -38,8 +107,16 @@ class SettingsFragment : ScribeFragment("Settings") {
         requireActivity().recreate()
     }
 
+    private fun navigateToKeyboardSettings() {
+        Intent(ACTION_INPUT_METHOD_SETTINGS).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(this)
+        }
+    }
+
     override fun onResume() {
         super.onResume()
         (activity as MainActivity).showHint("hint_shown_settings", R.string.app_settings_app_hint)
+        checkKeyboardInstallation()
     }
 }
--- app/src/main/java/be/scri/ui/screens/SettingsScreen.kt
+++ app/src/main/java/be/scri/ui/screens/SettingsScreen.kt
@@ -1,183 +1,264 @@
-package be.scri.ui.screens
-
-import android.util.Log
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.compose.foundation.Image
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.inputmethod.InputMethodManager
 import androidx.compose.foundation.background
-import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.scale
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import be.scri.R
 
+@SuppressLint("ComposeModifierMissing")
 @Composable
-fun SettingsScreen(modifier: Modifier = Modifier) {
-    var isDarkMode by remember {
-        mutableStateOf(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES)
-    }
-    Log.i("MY-TAG","The current mode is $isDarkMode")
-    Column(
-        modifier
-            .fillMaxSize()
-            .background(colorResource(R.color.you_background_color))
-            .padding(16.dp),
-    ) {
-        Text(
-            text = stringResource(R.string.app_settings_menu_title),
-            color = colorResource(R.color.app_text_color),
-            fontWeight = FontWeight.Bold,
-            fontSize = 24.sp,
-            modifier = Modifier.padding(bottom = 17.dp),
-        )
-        LazyColumn(
-            modifier = modifier.clip(shape = RoundedCornerShape(10.dp)),
-        ) {
-            item {
-                TableRow(
-                    title = stringResource(R.string.app_settings_menu_app_language),
-                    subtitle = stringResource(R.string.app_settings_menu_app_language_description),
-                )
-            }
-            item {
-                SwitchItem(
-                    title = stringResource(R.string.app_settings_menu_app_color_mode),
-                    subtitle = stringResource(R.string.app_settings_menu_app_color_mode_description),
-                    condition = isNightMode(),
-                    onSwitchChange = { isChecked ->
-                        AppCompatDelegate.setDefaultNightMode(
-                            if (isChecked) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO
-                        )
-                    }
-                )
-
-            }
-            item {
-                SwitchItem(
-                    title = stringResource(R.string.app_settings_menu_app_color_mode),
-                    subtitle = stringResource(R.string.app_settings_menu_app_color_mode_description),
-                    condition = isDarkMode,
-                    onSwitchChange = { isChecked ->
-                        isDarkMode = isChecked
-                        AppCompatDelegate.setDefaultNightMode(
-                            if (isChecked) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO
-                        )
-                    }
-                )
-            }
-            item {
-                SwitchItem(
-                    title = stringResource(R.string.app_settings_keyboard_functionality_popup_on_keypress),
-                    subtitle = stringResource(R.string.app_settings_keyboard_functionality_popup_on_keypress_description),
-                    condition = true
-                )
-            }
-        }
-    }
-}
-
-@Composable
-fun TableRow(
-    title: String,
-    subtitle: String,
+fun SettingsScreen(
+    onLanguageSelect: () -> Unit,
+    onDarkModeChange: (Boolean) -> Unit,
+    onInstallKeyboard: () -> Unit,
+    isKeyboardInstalled: Boolean,
+    defaultDarkMode: Boolean
 ) {
-    Column(modifier = Modifier.fillMaxWidth().background(colorResource(R.color.card_view_color))) {
-        Spacer(modifier = Modifier.size(10.dp))
-        Row(
-            modifier =
-                Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp),
+    val context = LocalContext.current
+    val scrollState = rememberScrollState()
+    var isUserDarkMode by remember { mutableStateOf(defaultDarkMode) }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+            .verticalScroll(scrollState)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
         ) {
             Text(
-                text = title,
-                fontSize = Dimensions.TextSizeMedium,
+                text = stringResource(id = R.string.app_settings_menu_title),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        top = 26.dp,
+                        start = 10.dp,
+                        end = 10.dp,
+                        bottom = 10.dp
+                    ),
+                color = MaterialTheme.colorScheme.onSurface,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
             )
-            Spacer(Modifier.width(215.dp))
-            Image(
-                painter = painterResource(R.drawable.right_arrow),
-                contentDescription = "Right Arrow",
-                modifier = Modifier.size(20.dp),
-            )
+            Surface(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp),
+                shape = RoundedCornerShape(8.dp),
+                color = MaterialTheme.colorScheme.surface
+            ) {
+                Column {
+                    SettingItem(
+                        title = stringResource(id = R.string.app_settings_menu_app_language),
+                        description = stringResource(id = R.string.app_settings_menu_app_language_description),
+                        onClick = onLanguageSelect
+                    )
+                    SwitchSettingItem(
+                        title = stringResource(id = R.string.app_settings_menu_app_color_mode),
+                        description = stringResource(id = R.string.app_settings_menu_app_color_mode_description),
+                        isChecked = isUserDarkMode,
+                        onCheckedChange = { isDarkMode ->
+                            isUserDarkMode = isDarkMode
+                            onDarkModeChange(isDarkMode)
+                        }
+                    )
+                }
+            }
+
+            if (isKeyboardInstalled) {
+                KeyboardLanguagesSection()
+            } else {
+                InstallKeyboardButton(onInstallKeyboard)
+            }
         }
+    }
+}
+
+@Composable
+private fun KeyboardLanguagesSection() {
+    val context = LocalContext.current
+    val languages = remember { getKeyboardLanguages(context) }
+
+    Column {
         Text(
-            modifier = Modifier.padding(horizontal = 16.dp),
-            text = subtitle,
-            fontSize = Dimensions.TextSizeSmall,
-            color = colorResource(R.color.light_special_key_color),
+            text = stringResource(id = R.string.app_settings_keyboard_title),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    top = 10.dp,
+                    start = 10.dp,
+                    end = 12.dp,
+                    bottom = 10.dp
+                ),
+            color = MaterialTheme.colorScheme.onSurface,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp),
+            shape = RoundedCornerShape(8.dp),
+            color = MaterialTheme.colorScheme.surface
+        ) {
+            Column {
+                languages.forEach { language ->
+                    LanguageItem(
+                        language = getLocalizedLanguageName(context, language),
+                        onClick = { /* Navigate to language settings */ }
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun InstallKeyboardButton(onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(80.dp)
+            .padding(
+                start = 13.dp,
+                end = 16.dp,
+                bottom = 16.dp
+            ),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = MaterialTheme.colorScheme.onPrimary
+        ),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Text(
+            text = stringResource(id = R.string.app_settings_button_install_keyboards),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
         )
     }
 }
 
 @Composable
-fun SwitchItem(
-    title: String,
-    subtitle: String,
-    condition : Boolean,
-    onSwitchChange: (Boolean) -> Unit = {},
-) {
-    var checked by remember { mutableStateOf(condition) }
-
-    Column(modifier = Modifier.fillMaxWidth().background(colorResource(R.color.card_view_color))) {
-        Spacer(modifier = Modifier.size(10.dp))
-        Row(
-            modifier =
-                Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp),
-        ) {
-            Column {
-                Text(
-                    text = title,
-                    fontSize = Dimensions.TextSizeMedium,
-                )
-                Text(
-                    text = subtitle,
-                    fontSize = Dimensions.TextSizeSmall,
-                    color = colorResource(R.color.light_special_key_color),
-                )
-            }
-            Spacer(modifier = Modifier.width(50.dp))
-            Switch(
-                checked = checked,
-                onCheckedChange = {
-                    checked = it
-                    onSwitchChange(it)
-                },
-                modifier = Modifier.scale(0.85f),
-                colors =
-                    SwitchDefaults.colors(
-                        checkedThumbColor = colorResource(R.color.switch_thumb_selector_color_true),
-                        uncheckedThumbColor = colorResource(R.color.switch_thumb_selector_color_false),
-                        checkedTrackColor = colorResource(R.color.switch_selector_color),
-                        uncheckedTrackColor = colorResource(R.color.switch_selector_color_false),
-                    ),
-            )
-        }
+private fun SettingItem(title: String, description: String, onClick: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(onClick = onClick)
+            .padding(16.dp)
+    ) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.titleMedium.copy(
+                fontWeight = FontWeight.Bold
+            ),
+            color = MaterialTheme.colorScheme.onSurface
+        )
+        Text(
+            text = description,
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+            modifier = Modifier.padding(top = 4.dp)
+        )
     }
 }
 
+@Composable
+private fun SwitchSettingItem(
+    title: String,
+    description: String,
+    isChecked: Boolean,
+    onCheckedChange: (Boolean) -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.titleMedium.copy(
+                fontWeight = FontWeight.Bold
+            ),
+            color = MaterialTheme.colorScheme.onSurface
+        )
+        Text(
+            text = description,
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+            modifier = Modifier.padding(top = 4.dp)
+        )
+        Switch(
+            checked = isChecked,
+            onCheckedChange = onCheckedChange,
+            modifier = Modifier.padding(top = 8.dp)
+        )
+    }
+}
 
 @Composable
-private fun isNightMode(): Boolean {
-    val currentMode = remember { mutableStateOf(AppCompatDelegate.getDefaultNightMode()) }
-    LaunchedEffect(Unit) {
-        currentMode.value = AppCompatDelegate.getDefaultNightMode()
+private fun LanguageItem(language: String, onClick: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(onClick = onClick)
+            .padding(16.dp)
+    ) {
+        Text(
+            text = language,
+            style = MaterialTheme.typography.titleMedium.copy(
+                fontWeight = FontWeight.Bold
+            ),
+            color = MaterialTheme.colorScheme.onSurface
+        )
     }
-    return when (currentMode.value) {
-        AppCompatDelegate.MODE_NIGHT_NO -> false
-        AppCompatDelegate.MODE_NIGHT_YES -> true
-        else -> isSystemInDarkTheme()
+}
+
+private fun getLocalizedLanguageName(context: Context, language: String): String {
+    val resourceId = when (language) {
+        "English" -> R.string.app__global_english
+        "French" -> R.string.app__global_french
+        "German" -> R.string.app__global_german
+        "Russian" -> R.string.app__global_russian
+        "Spanish" -> R.string.app__global_spanish
+        "Italian" -> R.string.app__global_italian
+        "Portuguese" -> R.string.app__global_portuguese
+        "Swedish" -> R.string.app__global_swedish
+        else -> return language
+    }
+    return context.getString(resourceId)
+}
+
+private fun getKeyboardLanguages(context: Context): List<String> {
+    val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    return imm.enabledInputMethodList.mapNotNull { inputMethod ->
+        when (inputMethod.serviceName) {
+            "be.scri.services.EnglishKeyboardIME" -> "English"
+            "be.scri.services.GermanKeyboardIME" -> "German"
+            "be.scri.services.RussianKeyboardIME" -> "Russian"
+            "be.scri.services.SpanishKeyboardIME" -> "Spanish"
+            "be.scri.services.FrenchKeyboardIME" -> "French"
+            "be.scri.services.ItalianKeyboardIME" -> "Italian"
+            "be.scri.services.PortugueseKeyboardIME" -> "Portuguese"
+            "be.scri.services.SwedishKeyboardIME" -> "Swedish"
+            else -> null
+        }
     }
 }
