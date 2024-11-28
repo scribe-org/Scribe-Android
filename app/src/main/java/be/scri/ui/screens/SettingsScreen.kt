f442cc8f8300aaf0b31e90c7d07e81b78c290c4e
angrezichatterbox <gouthammohanraj@gmail.com>
2024-11-28 18:51:06 +0530

    feat:Made the switch similar to the one in figma

--- app/src/main/java/be/scri/ui/screens/SettingsScreen.kt
+++ app/src/main/java/be/scri/ui/screens/SettingsScreen.kt
@@ -4,7 +4,10 @@ import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.focusable
+import androidx.compose.foundation.indication
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,7 +16,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.HorizontalDivider
@@ -32,6 +37,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.focusProperties
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
@@ -299,15 +305,32 @@ private fun SwitchSettingItem(
                 style = MaterialTheme.typography.bodyMedium,
             )
             Switch(
+                interactionSource = null,
                 checked = isChecked,
                 onCheckedChange = onCheckedChange,
-                modifier = Modifier.padding(start = 6.dp),
+                modifier = Modifier
+                    .width(51.dp)
+                    .height(31.dp),
+                thumbContent = {
+                    Box(
+                        modifier = Modifier
+                            .size(27.dp)
+                            .background(
+                                if (isChecked)
+                                    colorResource(R.color.switch_thumb_selector_color_true)
+                                else
+                                    colorResource(R.color.switch_thumb_selector_color_false)
+                                , shape = CircleShape
+                            )
+                    )
+                },
                 colors =
                     SwitchDefaults.colors(
                         checkedThumbColor = checkedThumbColor,
                         uncheckedThumbColor = uncheckedThumbColor,
                         checkedTrackColor = checkedTrackColor,
                         uncheckedTrackColor = uncheckedTrackColor,
+                        uncheckedBorderColor = Color.Transparent
                     ),
             )
         }
