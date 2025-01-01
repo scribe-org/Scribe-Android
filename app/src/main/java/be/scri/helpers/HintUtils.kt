6322d1f411a9f22877226a879a3acaab5ac58dd1
Akshay Waghmare <iec2022117@iiita.ac.in>
2025-01-02 00:20:19 +0530

    Updated placeholders and prompts in the command bar to use helper files.

--- app/src/main/java/be/scri/helpers/HintUtils.kt
+++ app/src/main/java/be/scri/helpers/HintUtils.kt
@@ -1,25 +1,17 @@
-/**
- * A helper to facilitate resetting application hints if the user would like to see them again.
- *
- * Copyright (C) 2024 Scribe
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
- */
-
 package be.scri.helpers
 
 import android.content.Context
+import android.util.Log
+import be.scri.services.SimpleKeyboardIME
+import be.scri.helpers.english.ENInterfaceVariables // Import for English variables
+import be.scri.helpers.french.FRInterfaceVariables
+import be.scri.helpers.german.DEInterfaceVariables
+import be.scri.helpers.italian.ITInterfaceVariables
+import be.scri.helpers.portugese.PTInterfaceVariables
+import be.scri.helpers.russian.RUInterfaceVariables
+import be.scri.helpers.spanish.ESInterfaceVariables
+import be.scri.helpers.swedish.SVInterfaceVariables
+
 
 object HintUtils {
     fun resetHints(context: Context) {
@@ -31,4 +23,118 @@ object HintUtils {
             apply()
         }
     }
+
+    fun getCommandBarHint(
+        currentState: SimpleKeyboardIME.ScribeState,
+        language: String
+    ): String {
+        // Determine the hint message
+        val hintMessage = when (currentState) {
+            SimpleKeyboardIME.ScribeState.TRANSLATE -> {
+                when (language) {
+                    "English" -> ENInterfaceVariables.TRANSLATE_PLACEHOLDER
+                    "French" -> FRInterfaceVariables.TRANSLATE_PLACEHOLDER
+                    "German" -> DEInterfaceVariables.TRANSLATE_PLACEHOLDER
+                    "Italian" -> ITInterfaceVariables.TRANSLATE_PLACEHOLDER
+                    "Portuguese" -> PTInterfaceVariables.TRANSLATE_PLACEHOLDER
+                    "Russian" -> RUInterfaceVariables.TRANSLATE_PLACEHOLDER
+                    "Spanish" -> ESInterfaceVariables.TRANSLATE_PLACEHOLDER
+                    "Swedish" -> SVInterfaceVariables.TRANSLATE_PLACEHOLDER
+                    else -> "" // Default fallback
+                }
+            }
+            SimpleKeyboardIME.ScribeState.CONJUGATE -> {
+                when (language) {
+                    "English" -> ENInterfaceVariables.CONJUGATE_PLACEHOLDER
+                    "French" -> FRInterfaceVariables.CONJUGATE_PLACEHOLDER
+                    "German" -> DEInterfaceVariables.CONJUGATE_PLACEHOLDER
+                    "Italian" -> ITInterfaceVariables.CONJUGATE_PLACEHOLDER
+                    "Portuguese" -> PTInterfaceVariables.CONJUGATE_PLACEHOLDER
+                    "Russian" -> RUInterfaceVariables.CONJUGATE_PLACEHOLDER
+                    "Spanish" -> ESInterfaceVariables.CONJUGATE_PLACEHOLDER
+                    "Swedish" -> SVInterfaceVariables.CONJUGATE_PLACEHOLDER
+                    else -> "" // Default fallback
+                }
+            }
+            SimpleKeyboardIME.ScribeState.PLURAL -> {
+                when (language) {
+                    "English" -> ENInterfaceVariables.PLURAL_PLACEHOLDER
+                    "French" -> FRInterfaceVariables.PLURAL_PLACEHOLDER
+                    "German" -> DEInterfaceVariables.PLURAL_PLACEHOLDER
+                    "Italian" -> ITInterfaceVariables.PLURAL_PLACEHOLDER
+                    "Portuguese" -> PTInterfaceVariables.PLURAL_PLACEHOLDER
+                    "Russian" -> RUInterfaceVariables.PLURAL_PLACEHOLDER
+                    "Spanish" -> ESInterfaceVariables.PLURAL_PLACEHOLDER
+                    "Swedish" -> SVInterfaceVariables.PLURAL_PLACEHOLDER
+                    else -> "" // Default fallback
+                }
+            }
+            else -> "" // Fallback for unknown states
+        }
+
+        return hintMessage
+    }
+
+
+    fun getPromptText(
+        currentState: SimpleKeyboardIME.ScribeState,
+        language: String
+    ): String {
+        return when (currentState) {
+
+            SimpleKeyboardIME.ScribeState.TRANSLATE -> {
+                val languageShorthand = mapOf(
+                    "English" to "en",
+                    "French" to "fr",
+                    "German" to "de",
+                    "Italian" to "it",
+                    "Portuguese" to "pt",
+                    "Russian" to "ru",
+                    "Spanish" to "es",
+                    "Swedish" to "sv"
+                )
+
+                val shorthand = languageShorthand[language] ?: "en" // Default fallback to "en"
+                "en -> $shorthand"
+            }
+
+            // CONJUGATE State
+            SimpleKeyboardIME.ScribeState.CONJUGATE -> {
+                when (language) {
+                    "English" -> ENInterfaceVariables.CONJUGATE_PROMPT
+                    "French" -> FRInterfaceVariables.CONJUGATE_PROMPT
+                    "German" -> DEInterfaceVariables.CONJUGATE_PROMPT
+                    "Italian" -> ITInterfaceVariables.CONJUGATE_PROMPT
+                    "Portuguese" -> PTInterfaceVariables.CONJUGATE_PROMPT
+                    "Russian" -> RUInterfaceVariables.CONJUGATE_PROMPT
+                    "Spanish" -> ESInterfaceVariables.CONJUGATE_PROMPT
+                    "Swedish" -> SVInterfaceVariables.CONJUGATE_PROMPT
+                    else -> "Conjugate :" // Default fallback
+                }
+            }
+
+            // PLURAL State
+            SimpleKeyboardIME.ScribeState.PLURAL -> {
+                when (language) {
+                    "English" -> ENInterfaceVariables.PLURAL_PROMPT
+                    "French" -> FRInterfaceVariables.PLURAL_PROMPT
+                    "German" -> DEInterfaceVariables.PLURAL_PROMPT
+                    "Italian" -> ITInterfaceVariables.PLURAL_PROMPT
+                    "Portuguese" -> PTInterfaceVariables.PLURAL_PROMPT
+                    "Russian" -> RUInterfaceVariables.PLURAL_PROMPT
+                    "Spanish" -> ESInterfaceVariables.PLURAL_PROMPT
+                    "Swedish" -> SVInterfaceVariables.PLURAL_PROMPT
+                    else -> "Plural :" // Default fallback
+                }
+            }
+
+            // Default fallback for unknown states
+            else -> ""
+        }
+    }
+
+
+
+
+
 }
