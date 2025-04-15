6abbe8773d26d7f067e1ef1e14bcd9cfb9192b3b
Femi Ogundipe <femimatt22@gmail.com>
2025-04-15 13:53:31 +0100

    fix: Remove suppress standard kdoc

--- app/src/main/java/be/scri/views/CustomDividerItemDecoration.kt
+++ app/src/main/java/be/scri/views/CustomDividerItemDecoration.kt
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-@file:Suppress("ktlint:standard:kdoc")
 
 import android.graphics.Canvas
 import android.graphics.Rect
--- app/src/main/java/be/scri/views/KeyboardView.kt
+++ app/src/main/java/be/scri/views/KeyboardView.kt
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-@file:Suppress("ktlint:standard:kdoc")
 
 package be.scri.views
 
