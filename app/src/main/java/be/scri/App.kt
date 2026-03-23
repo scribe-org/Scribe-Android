27b0112f653f2850b0b9608d6c5e7b6e512a9122
angrezichatterbox <gouthammohanraj@gmail.com>
2026-03-23 22:04:26 +0530

    fix:linting issues

--- app/src/main/java/be/scri/App.kt
+++ app/src/main/java/be/scri/App.kt
@@ -21,7 +21,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
