ab795bed8b21077b08798c496029a967238384c2
Femi Ogundipe <femimatt22@gmail.com>
2026-03-24 08:00:19 +0100

    Increase app text size in Conjugate MainActivity and fix the CI errors

--- app/src/conjugate/java/be/scri/activities/MainActivity.kt
+++ app/src/conjugate/java/be/scri/activities/MainActivity.kt
@@ -53,6 +53,13 @@ class MainActivity : ComponentActivity() {
                     )
                 }
 
+            val isIncreaseTextSize =
+                remember {
+                    mutableStateOf(
+                        PreferencesHelper.getIncreaseTextSizePreference(context),
+                    )
+                }
+
             val pagerState =
                 rememberPagerState {
                     screens.size
@@ -80,12 +87,18 @@ class MainActivity : ComponentActivity() {
 
             ScribeTheme(
                 useDarkTheme = isDarkMode.value,
+                isIncreaseTextSize = isIncreaseTextSize.value,
             ) {
                 ScribeApp(
                     pagerState = pagerState,
                     isDarkTheme = isDarkMode.value,
+                    isIncreaseTextSize = isIncreaseTextSize.value,
                     onDarkModeChange = { darkMode ->
                         updateTheme(darkMode)
+                    },
+                    onIncreaseTextSizeChange = { increaseTextSize ->
+                        PreferencesHelper.setIncreaseTextSizePreference(context, increaseTextSize)
+                        isIncreaseTextSize.value = increaseTextSize
                     },
                     resetHints = {
                         isHintChangedMap[0] = true
