5201654d57dc9a854da8bf8a2da5251c04a7962f
Prince Yadav <66916296+prince-0408@users.noreply.github.com>
2026-07-15 16:36:50 +0530

    Fix unresolved reference stringResource and restore dot-notation in layout XMLs

--- app/src/main/java/be/scri/ui/common/components/ClickableItemComp.kt
+++ app/src/main/java/be/scri/ui/common/components/ClickableItemComp.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
